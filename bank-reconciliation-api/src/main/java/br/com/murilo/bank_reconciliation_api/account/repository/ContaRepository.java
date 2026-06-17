6ff3487861c6efa962d82c65bd6bcd204c29de92
Murilo Besson <murilo.besson@outlook.com>
2026-06-17 11:57:31 -0300

    feat(api/account): adicione interface de repository

---
+++ bank-reconciliation-api/src/main/java/br/com/murilo/bank_reconciliation_api/account/repository/ContaRepository.java
@@ -0,0 +1,8 @@
+package br.com.murilo.bank_reconciliation_api.account.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContaRepository extends JpaRepository {
+}
