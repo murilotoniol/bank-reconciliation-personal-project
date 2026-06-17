c4bd1663c01b7ab990f18636ce4bcee86a0a94fa
Murilo Besson <murilo.besson@outlook.com>
2026-06-17 13:57:19 -0300

    refactor(account): adicione extensão correta de JPA

--- bank-reconciliation-api/src/main/java/br/com/murilo/bank_reconciliation_api/account/repository/ContaRepository.java
+++ bank-reconciliation-api/src/main/java/br/com/murilo/bank_reconciliation_api/account/repository/ContaRepository.java
@@ -1,8 +1,11 @@
 package br.com.murilo.bank_reconciliation_api.account.repository;
 
+import br.com.murilo.bank_reconciliation_api.account.entity.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ContaRepository extends JpaRepository {
+public interface ContaRepository extends JpaRepository<Conta, UUID>{
 }
